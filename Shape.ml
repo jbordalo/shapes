65613d6885e41081bdf3fda66e0e82e53464788c
jbordalo <j.bordalo@campus.fct.unl.pt>
2020-03-28 17:45:38 +0000

    Implement density
    Tested for union and intersection of rectangles

--- Shape.ml
+++ Shape.ml
@@ -59,7 +59,7 @@ let shapeIntersection = Intersection (rectOne, rectTwo);;
 
 let rec hasRect s =
     match s with
-          Rect (p,q) -> true
+		Rect (p,q) -> true
         | Circle (p,f) -> false
         | Union (l,r) -> hasRect l || hasRect r
         | Intersection (l,r) -> hasRect l || hasRect r
@@ -71,7 +71,7 @@ let rec hasRect s =
 
 let rec countBasic s =
     match s with
-          Rect (p,q) -> 1
+		Rect (p,q) -> 1
         | Circle (p,f) -> 1
         | Union (l,r) -> countBasic l + countBasic r
         | Intersection (l,r) -> countBasic l + countBasic r
@@ -84,14 +84,14 @@ let rec countBasic s =
 
 (* Distance between two points *)
 let dist p1 p2 =
-		sqrt ( (fst p1 -. fst p2)**2. +. (snd p1 -. snd p2)**2. )	
+	sqrt ( (fst p1 -. fst p2)**2. +. (snd p1 -. snd p2)**2. )	
 
 (* TODO Is it including all borders? *)
-let belongs p s =
+let rec belongs p s =
     match s with
-					(* Inclusive inequality due to border belonging to closed shape *)
-      		Rect (tl, br) -> fst p >= fst tl && fst p <= fst br && snd p >= snd tl && snd p <= snd br
-				| Circle (center, radius) -> dist p center <= radius
+		(* Inclusive inequality due to border belonging to closed shape *)
+      	Rect (tl, br) -> fst p >= fst tl && fst p <= fst br && snd p >= snd tl && snd p <= snd br
+		| Circle (center, radius) -> dist p center <= radius
         | Union (l,r) -> belongs p l || belongs p r
         | Intersection (l,r) -> belongs p l && belongs p r
         | Subtraction (l,r) -> belongs p l && not (belongs p r)
@@ -131,10 +131,33 @@ In a union, the two partial densities are added up if p is in the intersection z
 In an intersection, the two partial densities are added up if p is in the intersection zone; but the density is zero if p is outside the intersection zone.
 In a difference, the density remains if p is in the part of the shape that remains; the density is zero if p is in the part that represents erasure.
 *)
-let density p s =
-    0
+(* TODO Subtraction off the top of my head, may be wrong *)
+let rec density p s =
+    match s with
+      	Rect (_, _) -> if belongs p s then 1 else 0
+		| Circle (_, _) -> if belongs p s then 1 else 0
+        | Union (l,r) -> density p l + density p r
+        | Intersection (l,r) -> if belongs p s then density p l + density p r else 0
+        | Subtraction (l,r) -> if belongs p s then density p l else 0
 ;;
 
+(* Inside rectOne but not rectTwo = 1 *)
+(* density (2., 1.) shapeOverlapUnion *)
+(* Inside rectTwo but not rectOne = 1 *)
+(* density (3., 5.) shapeOverlapUnion *)
+(* Inside both = 2 *)
+(* density (3., 3.) shapeOverlapUnion *)
+(* Outside both = 0 *)
+(* density (0., 15.) shapeOverlapUnion *)
+
+(* Inside rectOne but not rectTwo = 0 *)
+(* density (2., 1.) shapeIntersection *)
+(* Inside rectTwo but not rectOne = 0 *)
+(* density (3., 5.) shapeIntersection *)
+(* Inside both = 2 *)
+(* density (3., 3.) shapeIntersection *)
+(* Outside both = 0 *)
+(* density (0., 15.) shapeIntersection *)
 
 (* FUNCTION which *)
 
@@ -159,7 +182,7 @@ let grid m n a b =
 
 (* FUNCTION countBasicRepetitions *)
 
-(* Para testar repetiï¿½ï¿½es, use a igualdade "=". Por exemplo, se houver dois cï¿½rculos iguais (com o mesmo centro e raio) e as restantes forma bï¿½sicas forem ï¿½nicas, entï¿½o o resultado serï¿½ 2. *)
+(* Para testar repetições, use a igualdade "=". Por exemplo, se houver dois círculos iguais (com o mesmo centro e raio) e as restantes forma básicas forem únicas, então o resultado será 2. *)
 
 let countBasicRepetitions s =
     0
